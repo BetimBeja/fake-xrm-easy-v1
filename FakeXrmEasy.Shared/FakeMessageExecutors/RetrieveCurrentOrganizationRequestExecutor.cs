0ecc25f6b66dab134bfd32d36c8688910cfe1f9b
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2020-12-10 22:57:29 +0100

    Improved RetrieveCurrentOrganizationRequestExecutor

--- FakeXrmEasy.Shared/FakeMessageExecutors/RetrieveCurrentOrganizationRequestExecutor.cs
+++ FakeXrmEasy.Shared/FakeMessageExecutors/RetrieveCurrentOrganizationRequestExecutor.cs
@@ -1,10 +1,9 @@
 ﻿#if FAKE_XRM_EASY_2015 || FAKE_XRM_EASY_2016 || FAKE_XRM_EASY_365 || FAKE_XRM_EASY_9
 using Microsoft.Xrm.Sdk;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Organization;
+using System.Linq;
 
 namespace FakeXrmEasy.FakeMessageExecutors
 {
@@ -18,11 +17,27 @@ namespace FakeXrmEasy.FakeMessageExecutors
         public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
         {
             var req = (RetrieveCurrentOrganizationRequest)request;
-
             OrganizationDetail organizationDetail = new OrganizationDetail();
+
+            Entity entity;
+            if (ctx.Data.ContainsKey("organization") &&
+               ctx.Data["organization"].Count > 0)
+            {
+                entity = ctx.CreateQuery("organization").First();
+            }
+            else
+            {
+                entity = new Entity("organization")
+                {
+                    Id = Guid.Parse("00000000-0000-0000-0000-000000000001")
+                };
+            }
+
+            organizationDetail.OrganizationId = entity.Id;
             organizationDetail.UrlName = "https://fakexrmeasy.crm.dynamics.com";
-            organizationDetail.UniqueName = "FakeXrmEasy";
-            organizationDetail.OrganizationVersion = "9.0.0.0";
+            organizationDetail.UniqueName = entity.GetAttributeValue<string>("name") ?? "FakeXrmEasy";
+            organizationDetail.OrganizationVersion = entity.GetAttributeValue<string>("initialversion") ?? "9.0.0.0";
+            organizationDetail.State = entity.GetAttributeValue<OrganizationState>("organizationstate");
 
             return new RetrieveCurrentOrganizationResponse
             {
@@ -31,7 +46,6 @@ namespace FakeXrmEasy.FakeMessageExecutors
                     { "Detail", organizationDetail }
                 }
             };
-            //throw new NotImplementedException();
         }
 
         public Type GetResponsibleRequestType()
---
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/RetrieveCurrentOrganizationRequestTests/RetrieveCurrentOrganizationRequestTests.cs
@@ -0,0 +1,48 @@
+﻿#if !FAKE_XRM_EASY && !FAKE_XRM_EASY_2013
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using System;
+using Xunit;
+
+namespace FakeXrmEasy.Tests.FakeContextTests.RetrieveCurrentOrganizationRequestTests
+{
+    public class RetrieveCurrentOrganizationRequestTests
+    {
+        [Fact]
+        public void When_retrieve_current_organization_is_called_without_context_default_organization_is_returned()
+        {
+            var context = new XrmFakedContext();
+            var service = context.GetOrganizationService();
+
+            var request = new RetrieveCurrentOrganizationRequest();
+
+            var response = service.Execute(request) as RetrieveCurrentOrganizationResponse;
+
+            Assert.Equal("FakeXrmEasy", response.Detail.UniqueName);
+            Assert.Equal("9.0.0.0", response.Detail.OrganizationVersion);
+        }
+
+        [Fact]
+        public void When_retrieve_current_organization_is_called_with_context_correct_organization_is_returned()
+        {
+            var context = new XrmFakedContext();
+            var service = context.GetOrganizationService();
+
+            var organization = new Entity("organization")
+            {
+                Id = Guid.NewGuid(),
+                ["name"] = "AlbanianXrm",
+                ["initialversion"] = "8.0.0.0"
+            };
+            context.Initialize(organization);
+
+            var request = new RetrieveCurrentOrganizationRequest();
+
+            var response = service.Execute(request) as RetrieveCurrentOrganizationResponse;
+
+            Assert.Equal("AlbanianXrm", response.Detail.UniqueName);
+            Assert.Equal("8.0.0.0", response.Detail.OrganizationVersion);
+        }
+    }
+}
+#endif
